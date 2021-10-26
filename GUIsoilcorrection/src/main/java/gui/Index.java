5265ec64373bed01d986dd923d12236c3aaf8dd0
Gabriel Luís Lopes <80367902+gabrielluislopes@users.noreply.github.com>
2021-10-26 20:39:02 -0300

    Criação da tela principal
    A primeira escolha foi criar uma tela inicial para o projeto, com 4 opções de escolha, cada uma referente a uma função do sistema.

---
+++ GUIsoilcorrection/src/main/java/gui/Index.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gui;
+
+/**
+ *
+ * @author gabri
+ */
+public class Index {
+    
+    public static void main(String args[]){
+        MenuPrincipal mp = new MenuPrincipal();
+        mp.setVisible(true);
+    }
+    
+}
